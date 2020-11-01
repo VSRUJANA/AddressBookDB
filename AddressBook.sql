02e78a82c95a824f6ae4908cac8092842dce2059
VSRUJANA <srujana.valavala@gmail.com>
2020-11-02 00:26:54 +0530

    [srujana] Refactor. Retrieve contacts sorted alphabetically by person's name for a given city

--- AddressBook.sql
+++ AddressBook.sql
@@ -53,8 +53,7 @@ select City,count(City) from Address_Book group by City;
 -- Count contacts by State in Address_Book  
 select State,count(State) from Address_Book group by State;
 
-
-
-
-
-
+-- Sort contacts by first name for a given city
+select * from Address_Book
+where City = 'Manhattan'
+order by FirstName asc;
