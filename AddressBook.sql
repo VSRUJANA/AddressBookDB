d296f50f3343cc22aa1ae37faf411fe32a27667e
VSRUJANA <srujana.valavala@gmail.com>
2020-11-02 00:01:35 +0530

    [srujana] Refactor. Retrieve contacts belonging to a city or state

--- AddressBook.sql
+++ AddressBook.sql
@@ -44,6 +44,11 @@ delete Address_Book
 where FirstName = 'Howard';
 select* from Address_Book;
 
+-- Retrieve contacts belonging to City or state from Address_Book
+select * from Address_Book
+where City = 'NewYork City' or State = 'Florida';
+
+
 
 
 
