52f04128e2cc31797502bc25f5cc2e0daa8e4d48
VSRUJANA <srujana.valavala@gmail.com>
2020-11-01 23:39:50 +0530

    [srujana] Refactor. Address_Book table created in the database

--- AddressBook.sql
+++ AddressBook.sql
@@ -3,3 +3,17 @@ create database Address_Book_Service;
 -- View database name
 use Address_Book_Service;
 select DB_NAME() 
+
+-- Create table in the database
+create table Address_Book
+(
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+Address varchar(60) not null,
+City varchar(15) not null,
+State varchar(15) not null,
+Zipcode varchar(6) not null,
+PhoneNumber varchar(12) not null,
+Email varchar(25) not null
+);
+
